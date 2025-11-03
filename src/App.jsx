2374b088944e2db8a7e1a149f5e755f0f262e441
HoangNguyenKim <ngkimhoang05@gmail.com>
2025-11-03 22:07:11 +0700

    Video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () =>{
+  return (
+    <div class="name_bo">Kevin Kim Hoang</div>
+  );
+}
 const App = () => {
   const [count, setCount] = useState(0)
 
@@ -17,6 +22,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello Work</h1>
+      <MyComponent/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
