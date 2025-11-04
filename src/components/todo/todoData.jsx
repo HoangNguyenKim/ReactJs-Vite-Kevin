7575a8996c90c6ba011f41f4f5f87f6f86b62d00
HoangNguyenKim <ngkimhoang05@gmail.com>
2025-11-05 00:09:14 +0700

    'Video33'

---
+++ src/components/todo/todoData.jsx
@@ -0,0 +1,8 @@
+const TodoData = () => {
+    return (
+        <div className="todo_data">
+            <div > Watch youtube</div>
+        </div>
+    );
+}
+export default TodoData
---
+++ src/components/todo/todoNew.jsx
@@ -0,0 +1,9 @@
+const TodoNew = () => {
+    return (
+        <div className="todo_New">
+            <input type="text" />
+            <button>Add</button>
+        </div>
+    );
+}
+export default TodoNew;
