7e9aa08f19320f5d581c2aea46adf85fc3497040
HoangNguyenKim <ngkimhoang05@gmail.com>
2025-11-04 22:27:22 +0700

    Video30

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,7 +1,20 @@
 import './style.css';
+
+// const Hoang = "Kim Hoang"\;
+// const Hoang = 18;
+// const Hoang = true;
+// const Hoang = {
+//   name: "HoangKim",
+//   age :12
+// }
+// const Hoang = 
 const MyComponent = () =>{
   return (
-    <div className="name">Kevin Kim Hoang</div>
+    <>
+      <div className="name"> Kevin Kim Hoang</div>
+      <div className=""> {console.log("Nguyen Kim Hoang")}</div>
+    </>
+    
   );
 }
 export default MyComponent;
