465432acd858f180783f845492a082a19825d27e
HoangNguyenKim <ngkimhoang05@gmail.com>
2025-11-17 18:26:21 +0700

    'Video36'

--- src/components/todo/todoNew.jsx
+++ src/components/todo/todoNew.jsx
@@ -1,12 +1,22 @@
 const TodoNew = (props) => {
     console.log(props);
     const { callMe } = props;
-    callMe("Hoang");
+    const handelClick = () => {
+        console.log("click me ");
+    }
+    const handleOnchange = (name) => {
+        console.log("New value", name)
+    }
     return (
         <div className="todo_New">
-            <input type="text" />
-            <button>Add</button>
-        </div>
+            <input type="text"
+                onChange={(event) => { handleOnchange(event.target.value) }}
+            />
+            <button
+                style={{ cursor: 'pointer' }}
+                onClick={handelClick}
+            >Add</button>
+        </div >
     );
 }
 export default TodoNew;
