9f8a75920e25886d8e3db718ef4d25dd6db53b0b
HoangNguyenKim <ngkimhoang05@gmail.com>
2025-11-21 01:24:33 +0700

    'Video47'

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from './components/todo/todoData'
 import TodoNew from './components/todo/todoNew'
 import reactLogo from './assets/react.svg'
 import { useState } from 'react'
+import Header from './components/layout/header'
+import Footer from './components/layout/footer'
 const App = () => {
 
   const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
@@ -20,22 +22,26 @@ const App = () => {
   }
 
   return (
-    <div className="todo_container">
-      <div className="todo_title">ToDo List</div>
-      <TodoNew
-        addNewTodo={addNewTodo}
-      />
-      {toDoLists.length > 0 ?
-        <TodoData
-          toDoList={toDoLists}
-          deleteTodo={deleteTodo}
+    <>
+      <Header />
+      <div className="todo_container">
+        <div className="todo_title">ToDo List</div>
+        <TodoNew
+          addNewTodo={addNewTodo}
         />
-        :
-        <div>
-          <img src={reactLogo} className='logo' alt="" />
-        </div>
-      }
-    </div>
+        {toDoLists.length > 0 ?
+          <TodoData
+            toDoList={toDoLists}
+            deleteTodo={deleteTodo}
+          />
+          :
+          <div>
+            <img src={reactLogo} className='logo' alt="" />
+          </div>
+        }
+      </div>
+      <Footer />
+    </>
   )
 }
 
