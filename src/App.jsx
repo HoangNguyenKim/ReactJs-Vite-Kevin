b105b6f4ff066d9be1e65671721410ee42fa2994
HoangNguyenKim <ngkimhoang05@gmail.com>
2025-11-21 11:46:24 +0700

    'Video48'

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import reactLogo from './assets/react.svg'
 import { useState } from 'react'
 import Header from './components/layout/header'
 import Footer from './components/layout/footer'
+import { Outlet } from 'react-router-dom'
 const App = () => {
 
   const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
@@ -40,6 +41,7 @@ const App = () => {
           </div>
         }
       </div>
+      <Outlet />
       <Footer />
     </>
   )
---
+++ src/components/todo/todoData.jsx
@@ -0,0 +1,40 @@
+const TodoData=(props)=> {
+    const {
+        toDoList,
+        deleteTodo
+    }
+
+    =props;
+
+    const handleDelete=(idTodo)=> {
+        deleteTodo(idTodo)
+    }
+
+    return (<div className="todo_data" > {
+            toDoList.map((item, index, array)=> {
+                    return (<div className="todo-item" key= {
+                            item.id
+                        }
+
+                        > <div> {
+                            item.name
+                        }
+
+                        </div> <button style= {
+                                {
+                                cursor: "pointer"
+                            }
+                        }
+
+                        onClick= {
+                            ()=> handleDelete(item.id)
+                        }
+
+                        >delete </button> </div>);
+                })
+        }
+
+        </div >);
+}
+
+export default TodoData;
